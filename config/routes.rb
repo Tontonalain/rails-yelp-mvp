7a63b423dfacad47fbdbbc762e7b51110743683e
Alain Vallejo <alain.vallejo@laposte.net>
2022-11-08 13:55:38 -0400

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "restaurants", to: "restaurants#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "restaurants/new", to: "restaurants#new"
+
+  get "restaurants/:id", to: "restaurants#show"
+
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id/edit", to: "restaurants#edit"
+
+  get ""
 end
